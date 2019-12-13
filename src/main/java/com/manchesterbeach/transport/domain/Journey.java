c2ea6281ab5490ee75442139ca89f88436d2c3d6
alexwestcott <alexwestcott24@gmail.com>
2019-12-13 15:37:07 +0000

    [Alex] Fixes constructors for Lombok

--- src/main/java/com/manchesterbeach/transport/domain/Journey.java
+++ src/main/java/com/manchesterbeach/transport/domain/Journey.java
@@ -5,13 +5,8 @@ import lombok.Data;
 @Data
 public class Journey {
 
-    private String name;
-    private Station originStation;
-    private Station destinationStation;
+    private final String name;
+    private final Station originStation;
+    private final Station destinationStation;
 
-    public Journey(String name, Station originStation, Station destinationStation) {
-        this.name = name;
-        this.originStation = originStation;
-        this.destinationStation = destinationStation;
-    }
 }
--- src/main/java/com/manchesterbeach/transport/domain/Station.java
+++ src/main/java/com/manchesterbeach/transport/domain/Station.java
@@ -2,22 +2,18 @@ package com.manchesterbeach.transport.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Station {
 
     @JsonProperty("crs")
     private String id;
     private String name;
 
-    public Station(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Station(){
-
-    }
 }
