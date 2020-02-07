a56f0b570e362b7e76d1e9ed9761c510a66ca44e
Dan Ennis <dan.ennis@thoughtworks.com>
2020-02-07 14:29:55 +0000

    [DE][#02] Add cross origins to departures controller.

--- src/main/java/com/manchesterbeach/transport/controller/DeparturesController.java
+++ src/main/java/com/manchesterbeach/transport/controller/DeparturesController.java
@@ -4,10 +4,12 @@ import com.manchesterbeach.transport.service.DeparturesService;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:3000", "http://beach-train-app.herokuapp.com", "https://beach-train-app.herokuapp.com", "http://beach-train-app-qa.herokuapp.com", "https://beach-train-app-qa.herokuapp.com"})
 @RestController
 public class DeparturesController {
 
